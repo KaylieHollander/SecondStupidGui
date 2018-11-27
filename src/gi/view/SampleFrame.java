0970899a8074026b4dfd2b983f06b522fc8c35e6
Kaylie Hollander <Kphollander1@gmail.com>
2018-11-27 08:23:08 -0700

    The last commit the code to make the window did not go through. This is the code for the panel class and the frame class.

--- src/gi/view/SampleFrame.java
+++ src/gi/view/SampleFrame.java
@@ -1,6 +1,30 @@
 package gi.view;
 
-public class SampleFrame
-{
+import javax.swing.JFrame;
+import gui.controller.*;
 
+public class SampleFrame extends JFrame 
+{
+	private GuiController appController;
+	private SamplePanel appPanel;
+	
+	public SampleFrame(GuiController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new SamplePanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1000, 500);
+		this.setTitle("Second GUI App");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
+
---
+++ src/gi/view/SamplePanel.java
@@ -0,0 +1,35 @@
+package gi.view;
+
+import gui.controller.*;
+import javax.swing.*;
+
+public class SamplePanel extends JPanel
+{
+	private GuiController appController;
+	
+	public SamplePanel(GuiController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
